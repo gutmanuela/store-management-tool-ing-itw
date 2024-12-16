c6e2fed354282d687dfd9249df4ba41de8dc2813
gutmanuela <gut.manuelanicoleta@gmail.com>
2024-12-16 13:29:38 +0200

    integration and unit test for product service and controller were added

--- src/main/java/com/ing/store_management/model/dto/ProductDto.java
+++ src/main/java/com/ing/store_management/model/dto/ProductDto.java
@@ -7,10 +7,20 @@ public class ProductDto {
     private String code;
     private String categoryName;
 
+    public ProductDto() {
+    }
+
     public ProductDto(String code, double price, String name, int stockQuantity, String categoryName) {
         this.name = name;
         this.price = price;
         this.stockQuantity = stockQuantity;
+        this.code = code;
+        this.categoryName = categoryName;
+    }
+
+    public ProductDto(String code, String name,  double price,String categoryName) {
+        this.name = name;
+        this.price = price;
         this.code = code;
         this.categoryName = categoryName;
     }
